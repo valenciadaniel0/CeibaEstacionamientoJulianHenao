278fe2b34bdf794fdeedafc2121253cdb1fa5e35
JULIANCHO923 <julianhenao92@gmail.com>
2018-08-22 16:39:08 -0500

    Primera prueba consultando servicios

--- src/test/java/co/com/ceiba/estacionamiento/julian/henao/testIntegracionConsultaServicios/TestRegistrosParqueadero.java
+++ src/test/java/co/com/ceiba/estacionamiento/julian/henao/testIntegracionConsultaServicios/TestRegistrosParqueadero.java
@@ -1,21 +1,24 @@
-package testIntegracionConsultas;
+package co.com.ceiba.estacionamiento.julian.henao.testIntegracionConsultaServicios;
+
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import co.com.ceiba.estacionamiento.julian.henao.controlador.ControladorParqueaderoRegistro;
 import co.com.ceiba.estacionamiento.julian.henao.modelo.ModeloParqueaderoRegistro;
 
 @RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestRegistrosParqueadero {
 
@@ -23,10 +26,10 @@ public class TestRegistrosParqueadero {
 	ControladorParqueaderoRegistro controladorParqueaderoRegistro;
 	
 	@Test
-	public void test() {
-		
-		ResponseEntity<List<ModeloParqueaderoRegistro>> responseEntity;
-		
+	@SqlGroup(@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:iniciandoBD.sql"))
+	public void RealizaConsultaRegistros() {		
+		ResponseEntity<List<ModeloParqueaderoRegistro>> responseEntity = controladorParqueaderoRegistro.obtenerRegistrosAnteriores();		
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());		
 	}
 
 }
